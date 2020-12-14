3df927e500ef72775760638d251c16749437e1ef
Jakub Bednar <jakub.bednar@gmail.com>
2020-12-14 10:34:18 +0100

    feat: added Tail function

---
+++ flux-dsl/src/main/java/com/influxdb/query/dsl/functions/TailFlux.java
@@ -0,0 +1,86 @@
+/*
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.influxdb.query.dsl.functions;
+
+import javax.annotation.Nonnull;
+
+import com.influxdb.Arguments;
+import com.influxdb.query.dsl.Flux;
+
+/**
+ * Tail caps the number of records in output tables to a fixed size <i>n</i>.
+ * <a href="http://bit.ly/flux-spec#tail">See SPEC</a>.
+ *
+ * <h3>Options</h3>
+ * <ul>
+ * <li><b>n</b> - The maximum number of records to output [int].</li>
+ * <li><b>offset</b> - The number of records to skip per table [int]. Default to <i>0</i>.</li>
+ * </ul>
+ *
+ * <h3>Example</h3>
+ * <pre>
+ * Flux flux = Flux
+ *     .from("telegraf")
+ *     .tail(5);
+ * </pre>
+ *
+ * @author Jakub Bednar (14/12/2020 10:20)
+ */
+public final class TailFlux extends AbstractParametrizedFlux {
+    public TailFlux(@Nonnull final Flux flux) {
+        super(flux);
+    }
+
+    @Nonnull
+    @Override
+    protected String operatorName() {
+        return "tail";
+    }
+
+    /**
+     * @param numberOfResults The number of results
+     * @return this
+     */
+    @Nonnull
+    public TailFlux withN(final int numberOfResults) {
+
+        Arguments.checkPositiveNumber(numberOfResults, "Number of results");
+
+        this.withPropertyValue("n", numberOfResults);
+
+        return this;
+    }
+
+    /**
+     * @param offset The number of records to skip per table.
+     * @return this
+     */
+    @Nonnull
+    public TailFlux withOffset(final int offset) {
+
+        Arguments.checkNotNegativeNumber(offset, "The number of records to skip");
+
+        this.withPropertyValue("offset", offset);
+
+        return this;
+    }
+}
---
+++ flux-dsl/src/test/java/com/influxdb/query/dsl/functions/TailFluxTest.java
@@ -0,0 +1,100 @@
+/*
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.influxdb.query.dsl.functions;
+
+import java.util.HashMap;
+
+import com.influxdb.query.dsl.Flux;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jakub Bednar (14/12/2020 10:24)
+ */
+@RunWith(JUnitPlatform.class)
+class TailFluxTest
+{
+	@Test
+	void tail() {
+
+		Flux flux = Flux
+				.from("telegraf")
+				.tail(5);
+
+		Assertions.assertThat(flux.toString()).isEqualToIgnoringWhitespace("from(bucket:\"telegraf\") |> tail(n: 5)");
+	}
+
+	@Test
+	void tailOffset() {
+
+		Flux flux = Flux
+				.from("telegraf")
+				.tail(100, 10);
+
+		Assertions.assertThat(flux.toString())
+				.isEqualToIgnoringWhitespace("from(bucket:\"telegraf\") |> tail(n: 100, offset:10)");
+	}
+
+	@Test
+	void tailOffsetZero() {
+
+		Flux flux = Flux
+				.from("telegraf")
+				.tail(100, 0);
+
+		Assertions.assertThat(flux.toString())
+				.isEqualToIgnoringWhitespace("from(bucket:\"telegraf\") |> tail(n: 100, offset:0)");
+	}
+
+	@Test
+	void tailPositive() {
+		Assertions.assertThatThrownBy(() -> Flux.from("telegraf").tail(-5))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("Expecting a positive number for Number of results");
+	}
+
+	@Test
+	void tailByParameter() {
+
+		Flux flux = Flux
+				.from("telegraf")
+				.tail()
+				.withPropertyNamed("n", "tail");
+
+		HashMap<String, Object> parameters = new HashMap<>();
+		parameters.put("tail", 15);
+
+		Assertions.assertThat(flux.toString(parameters))
+				.isEqualToIgnoringWhitespace("from(bucket:\"telegraf\") |> tail(n: 15)");
+	}
+
+	@Test
+	void tailByParameterMissing() {
+
+		Assertions.assertThatThrownBy(() -> Flux.from("telegraf").tail().withPropertyNamed("tail").toString())
+				.isInstanceOf(IllegalStateException.class)
+				.hasMessage("The parameter 'tail' is not defined.");
+	}
+}
