50c58179835e1f3757eb4634e6e0e9e036cf3b3b
Jakub Bednář <jakub.bednar@gmail.com>
2021-03-18 10:22:30 +0100

    chore: updated Swagger API to latest version (#206)

--- client/src/generated/java/com/influxdb/client/domain/BucketRetentionRules.java
+++ client/src/generated/java/com/influxdb/client/domain/BucketRetentionRules.java
@@ -13,16 +13,15 @@
 
 package com.influxdb.client.domain;
 
+import java.io.IOException;
 import java.util.Objects;
-import java.util.Arrays;
+
 import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import java.io.IOException;
 
 /**
  * BucketRetentionRules
@@ -82,6 +81,10 @@ public class BucketRetentionRules {
   @SerializedName(SERIALIZED_NAME_EVERY_SECONDS)
   private Integer everySeconds;
 
+  public static final String SERIALIZED_NAME_SHARD_GROUP_DURATION_SECONDS = "shardGroupDurationSeconds";
+  @SerializedName(SERIALIZED_NAME_SHARD_GROUP_DURATION_SECONDS)
+  private Long shardGroupDurationSeconds;
+
    /**
    * Get type
    * @return type
@@ -97,17 +100,35 @@ public class BucketRetentionRules {
   }
 
    /**
-   * Duration in seconds for how long data will be kept in the database.
-   * minimum: 1
+   * Duration in seconds for how long data will be kept in the database. 0 means infinite.
+   * minimum: 0
    * @return everySeconds
   **/
-  @ApiModelProperty(example = "86400", required = true, value = "Duration in seconds for how long data will be kept in the database.")
+  @ApiModelProperty(example = "86400", required = true, value = "Duration in seconds for how long data will be kept in the database. 0 means infinite.")
   public Integer getEverySeconds() {
     return everySeconds;
   }
 
   public void setEverySeconds(Integer everySeconds) {
     this.everySeconds = everySeconds;
+  }
+
+  public BucketRetentionRules shardGroupDurationSeconds(Long shardGroupDurationSeconds) {
+    this.shardGroupDurationSeconds = shardGroupDurationSeconds;
+    return this;
+  }
+
+   /**
+   * Shard duration measured in seconds.
+   * @return shardGroupDurationSeconds
+  **/
+  @ApiModelProperty(value = "Shard duration measured in seconds.")
+  public Long getShardGroupDurationSeconds() {
+    return shardGroupDurationSeconds;
+  }
+
+  public void setShardGroupDurationSeconds(Long shardGroupDurationSeconds) {
+    this.shardGroupDurationSeconds = shardGroupDurationSeconds;
   }
 
 
@@ -121,12 +142,13 @@ public class BucketRetentionRules {
     }
     BucketRetentionRules postBucketRequestRetentionRules = (BucketRetentionRules) o;
     return Objects.equals(this.type, postBucketRequestRetentionRules.type) &&
-        Objects.equals(this.everySeconds, postBucketRequestRetentionRules.everySeconds);
+        Objects.equals(this.everySeconds, postBucketRequestRetentionRules.everySeconds) &&
+        Objects.equals(this.shardGroupDurationSeconds, postBucketRequestRetentionRules.shardGroupDurationSeconds);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(type, everySeconds);
+    return Objects.hash(type, everySeconds, shardGroupDurationSeconds);
   }
 
 
@@ -136,6 +158,7 @@ public class BucketRetentionRules {
     sb.append("class BucketRetentionRules {\n");
     sb.append("    type: ").append(toIndentedString(type)).append("\n");
     sb.append("    everySeconds: ").append(toIndentedString(everySeconds)).append("\n");
+    sb.append("    shardGroupDurationSeconds: ").append(toIndentedString(shardGroupDurationSeconds)).append("\n");
     sb.append("}");
     return sb.toString();
   }
