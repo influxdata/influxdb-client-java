da7c88788e39984d1f0b0eceeddf09d298c0da8f
zhujunqi <junqi.zhu@coollu.com.cn>
2021-06-09 18:25:32 +0800

    Fix Checkstyle errors

--- client-core/src/main/java/com/influxdb/query/internal/FluxResultMapper.java
+++ client-core/src/main/java/com/influxdb/query/internal/FluxResultMapper.java
@@ -26,7 +26,6 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -98,7 +97,7 @@ public class FluxResultMapper {
         }
     }
 
-    private String camelCaseToSnakeCase(String str) {
+    private String camelCaseToSnakeCase(final String str) {
         return CAMEL_CASE_TO_SNAKE_CASE_REPLACE_PATTERN.matcher(str)
             .replaceAll("_$0")
             .toLowerCase();
