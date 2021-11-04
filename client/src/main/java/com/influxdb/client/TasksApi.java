0aee0741654bb8476efa0cb57a6fa1a1e6a1d7a2
Lonelyleaf <499904394@qq.com>
2021-11-04 21:33:48 +0800

    Update client/src/main/java/com/influxdb/client/TasksApi.java
    Co-authored-by: Jakub Bednář <jakub.bednar@gmail.com>

--- client/src/main/java/com/influxdb/client/TasksApi.java
+++ client/src/main/java/com/influxdb/client/TasksApi.java
@@ -273,7 +273,7 @@ public interface TasksApi {
      * @return A list of tasks
      */
     @Nonnull
-    List<Task> findTasks(@NotNull TaskQuery query);
+    List<Task> findTasks(@Nonnull final TaskQuery query);
 
     /**
      * List all task members.
