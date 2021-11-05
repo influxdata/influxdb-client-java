d49099716349c1ff65c372a1270654732fa834f7
Lonelyleaf <499904394@qq.com>
2021-11-05 15:42:10 +0800

    Update client/src/main/java/com/influxdb/client/BucketsQuery.java
    Co-authored-by: Jakub Bednář <jakub.bednar@gmail.com>

--- client/src/main/java/com/influxdb/client/BucketsQuery.java
+++ client/src/main/java/com/influxdb/client/BucketsQuery.java
@@ -17,7 +17,8 @@ public class BucketsQuery {
     private Integer limit;
 
     /**
-     * The last resource ID from which to seek from (but not including). This is to be used instead of &#x60;offset&#x60;. (optional)
+     * The last resource ID from which to seek from (but not including). 
+     * This is to be used instead of &#x60;offset&#x60;. (optional)
      */
     @Nullable
     private String after;
