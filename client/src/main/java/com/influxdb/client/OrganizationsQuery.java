ecf0fc95230eceb65292139f377bc967c64e0d3e
Lonelyleaf <499904394@qq.com>
2021-11-05 15:42:17 +0800

    Update client/src/main/java/com/influxdb/client/OrganizationsQuery.java
    Co-authored-by: Jakub Bednář <jakub.bednar@gmail.com>

--- client/src/main/java/com/influxdb/client/OrganizationsQuery.java
+++ client/src/main/java/com/influxdb/client/OrganizationsQuery.java
@@ -11,7 +11,7 @@ public class OrganizationsQuery {
     private Integer offset;
 
     /**
-     * limit  (optional, default to 20)
+     * Limit. (optional, default to 20)
      */
     @Nullable
     private Integer limit;
