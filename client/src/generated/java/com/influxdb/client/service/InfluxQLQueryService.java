3b10b5381e604ad36f6dc681cc81ee4d82b55884
Jakub Bednar <jakub.bednar@gmail.com>
2022-08-03 08:31:43 +0200

    refactor: InfluxQLQueryService is generated by apigen

--- client/src/generated/java/com/influxdb/client/service/InfluxQLQueryService.java
+++ client/src/generated/java/com/influxdb/client/service/InfluxQLQueryService.java
@@ -8,21 +8,20 @@ import retrofit2.http.*;
 
 public interface InfluxQLQueryService {
 
-	/**
-	 * @param db Bucket to query (required)
-	 * @param query (required)
-	 * @param retentionPolicy Retention policy name (optional)
-	 * @param zapTraceSpan OpenTracing span context (optional)
-	 * @return response in csv format
-	 */
-	@Headers({ "Accept:application/csv", "Content-Type:application/vnd.influxql" })
-	@POST("query")
-	Call<ResponseBody> query(
-			@Body String query,
-			@Nonnull @Query("db") String db,
-			@Query("rp") String retentionPolicy,
-			@Query("epoch") String epoch,
-			@Header("Zap-Trace-Span") String zapTraceSpan
-	);
-
+    /**
+     * @param db              Bucket to query (required)
+     * @param query           (required)
+     * @param retentionPolicy Retention policy name (optional)
+     * @param zapTraceSpan    OpenTracing span context (optional)
+     * @return response in csv format
+     */
+    @Headers({"Accept:application/csv", "Content-Type:application/vnd.influxql"})
+    @POST("query")
+    Call<ResponseBody> query(
+            @Body String query,
+            @Nonnull @Query("db") String db,
+            @Query("rp") String retentionPolicy,
+            @Query("epoch") String epoch,
+            @Header("Zap-Trace-Span") String zapTraceSpan
+    );
 }
